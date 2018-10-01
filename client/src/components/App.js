2413c254f3764da5d555159fbd29aad84ba1c1f0
Sanderhelleso <sanderhelleso@hotmail.com>
2018-10-01 13:45:18 -0700

    Initial commit

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import BarChart from './charts/BarChart';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+            <h1>Hello World</h1>
+            <BarChart />
+      </div>
+    )
+  }
+}
---
+++ client/src/components/charts/BarChart.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {Doughnut} from 'react-chartjs-2';
+
+export default class BarChart extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
